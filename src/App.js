330e44a590e10a7dba906eea13e90d0eaa27afe9
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-07-02 16:14:50 -0500

    build: installed firebase and setting up services

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
 import './App.css';
 
+// Import the functions you need from the SDKs you need
+
+import { collection, getFirestore} from 'firebase/firestore';
+
+import { initializeApp } from "firebase/app";
+
+
+// specialized firebaseconfig object
+const firebaseConfig = {
+  apiKey: "AIzaSyCaCLZtFd9MB9f8AMfGdSFBYbfqMDQqhsA",
+  authDomain: "poker1-daf45.firebaseapp.com",
+  projectId: "poker1-daf45",
+  storageBucket: "poker1-daf45.appspot.com",
+  messagingSenderId: "1056178650590",
+  appId: "1:1056178650590:web:9f5b042e20ee8745eab14f"
+};
+
+
+// Initialize Firebase
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+// collection references
+
+const bestHandsColRef = collection(db, 'bestHands');
+
+
+// getBestHands 
+
+
+
+// App component rendered below
 function App() {
   return (
     <div className="App">
